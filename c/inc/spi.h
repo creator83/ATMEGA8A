4acf427264b343a23af53a79a9391267364a5114
Work_desktop <creator83@yandex.ru>
2015-10-22 09:57:55 +0300

    files

---
+++ c/inc/spi.h
@@ -0,0 +1,16 @@
+﻿/*
+ * spi.h
+ *
+ * Created: 22.10.2015 9:51:47
+ *  Author: User
+ */ 
+#include <avr/io.h>
+
+#ifndef SPI_H_
+#define SPI_H_
+
+
+
+
+
+#endif /* SPI_H_ */
---
+++ c/prj/use_spi.c
@@ -0,0 +1,15 @@
+﻿/*
+ * use_spi.c
+ *
+ * Created: 22.10.2015 9:50:45
+ *  Author: User
+ */ 
+#include <avr/io.h>
+
+int main(void)
+{
+	while(1)
+	{
+		//TODO:: Please write your application code
+	}
+}
